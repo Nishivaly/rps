a61d1edf65840a6fb4154b591367cd216ada6495
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-15 13:52:17 -0300

    Add function to decide winner of the round

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ console.log("Human choice is: " + humanChoice);
 let computerChoice = getComputerChoice();
 console.log("Computer choice is: " + computerChoice);
 
+decideWinner(humanChoice, computerChoice);
+
 function getHumanChoice() {
     let playerChoice = prompt("Choose rock, paper or scissors");
     while (true) {
@@ -23,4 +25,38 @@ function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
+}
+
+function decideWinner(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice == "paper") {
+                console.log("You lose");
+            } else if (computerChoice == "scissors") {
+                console.log("You win");
+            } else {
+                console.log("You tie");
+            }
+            break;
+
+        case "paper":
+            if (computerChoice == "scissors") {
+                console.log("You lose");
+            } else if (computerChoice == "rock") {
+                console.log("You win");
+            } else {
+                console.log("You tie");
+            }
+            break;
+
+        case "scissors":
+            if (computerChoice == "rock") {
+                console.log("You lose");
+            } else if (computerChoice == "paper") {
+                console.log("You win");
+            } else {
+                console.log("You tie");
+            }
+            break;
+    }
 }
